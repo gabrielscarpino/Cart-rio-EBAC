5dd65fa2ca317fa1f450ccb0f6ff1e73aa891756
Gabriel Scarpino <gabrielscarpino@hotmail.com>
2024-11-21 14:33:55 -0300

    Opção de saída
    Incluído no sistema a opção de saída do usuário.

--- Projeto Interface EBAC.cpp
+++ Projeto Interface EBAC.cpp
@@ -126,6 +126,7 @@ int main()
  	 printf("\t1 Registrar nomes\n");
 	 printf("\t2 Consultar nomes\n");
 	 printf("\t3 Deletar nomes\n\n");
+	 printf("\t4 Sair do sistema\n\n");
 	 printf("Opção: "); //Fim do menu
 	
 	 scanf("%d", &opcao); // Armazenado a escolha do usuário
@@ -145,6 +146,11 @@ int main()
 	case 3:
 	    deletar();
 		break;
+		 
+    case 4:
+    printf("Obrigado por usar o sistema");
+    return 0;
+    break;
 		
 	default:
 	   printf("Opção indisponivel\n");
